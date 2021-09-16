c5c1771da0bbf575cd04c4c415328d09ffc3ea03
jpedrofp <ferreirapillon.joaop@gmail.com>
2021-09-16 15:17:53 -0300

    inicial commit

--- tribruxo/src/components/Home/Home.jsx
+++ tribruxo/src/components/Home/Home.jsx
@@ -7,15 +7,31 @@ const Home = ({ characters }) => {
 
   function handleClick() {
     setTriBruxos([]);
-
+    const random = [];
+    const house = [];
     const random1 = Math.floor(Math.random() * (11 - 0) + 0);
     const random2 = Math.floor(Math.random() * (11 - 0) + 0);
     const random3 = Math.floor(Math.random() * (11 - 0) + 0);
 
+    random.push(random1);
     const selected1 = characters[random1];
-    const selected2 = characters[random2];
-    const selected3 = characters[random3];
-    setTriBruxos([...triBruxos, selected1, selected2, selected3]);
+    house.push(selected1.house);
+
+    if (
+      !random.includes(random2) &&
+      !house.includes(characters[random2].house)
+    ) {
+      random.push(random2);
+      const selected2 = characters[random2];
+      house.push(selected2.house);
+      if (
+        !random.includes(random3) &&
+        !house.includes(characters[random3].house)
+      ) {
+        const selected3 = characters[random3];
+        setTriBruxos([selected1, selected2, selected3]);
+      }
+    } else console.log("tryagain");
   }
 
   return (
@@ -24,13 +40,15 @@ const Home = ({ characters }) => {
       <div className="container">
         <h3>Clique no botão para encontrar os feiticeiros!</h3>
 
+   
         <button className="Start" onClick={handleClick}>
           Começar
-        </button>
-
+          </button>
         <div className="render">
           <Students characters={triBruxos}></Students>
         </div>
+       
+<h6>Obs: Se não encontrar os bruxos é por quê nenhum foi merecedor! Tente novamente!</h6>
       </div>
     </>
   );
